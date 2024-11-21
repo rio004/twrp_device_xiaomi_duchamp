9a397e1d7265f392e1eddf2f68dbbbfbe6c9331d
rio004 <riyon336@gmail.com>
2024-11-21 14:41:29 +0000

    duchamp: Set shipping API level
    Change-Id: I27ddb014836ce8a6b545fb6a7678bee90700af3a

---
+++ device.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 32
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
